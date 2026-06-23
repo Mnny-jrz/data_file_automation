901bf1f6a42922337aef8e4e914e05e010177aa3
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-23 20:15:20 +0800

    feat: implement read_numbers method

--- P3/main.py
+++ P3/main.py
@@ -1,3 +1,11 @@
 class NumberStats:
     def __init__(self, filename):
         self.filename = filename
+class NumberStats:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_numbers(self):
+        with open(self.filename, 'r') as file:
+            numbers = [int(num) for num in file.read().split()]
+        return numbers
