93476f3acfba8f390ed5777a445f01a852fc9eef
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-23 20:06:00 +0800

    feat: implement read_data method to load student records

--- P2/main.py
+++ P2/main.py
@@ -1,3 +1,11 @@
 class StudentGWA:
     def __init__(self, filename):
         self.filename = filename
+class StudentGWA:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_data(self):
+        with open(self.filename, 'r') as file:
+            data = [line.strip().split() for line in file.readlines()]
+        return data
